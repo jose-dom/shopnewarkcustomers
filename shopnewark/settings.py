d02c8ef3be6062d1eb1a6520d363e4c2d3432fa3
jose-dom <josedom000@gmail.com>
2020-06-19 12:31:00 -0400

    update

--- shopnewark/settings.py
+++ shopnewark/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['shopnewark-customers.herokuapp.com','*']
+ALLOWED_HOSTS = ['shopnewark-customers.herokuapp.com','*','shopnewark.herokuapp.com']
 
 
 # Application definition
